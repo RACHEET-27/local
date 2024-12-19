545834eb9b6cc33c92aab1bcd4e5cd5987c05587
Racheet Vatsa <racheet2003@gmail.com>
2024-12-19 06:13:29 +0530

    added rooms component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'hinv-root',
   templateUrl: './app.component.html',
+  // template: `<h1>Hello world from inline template</h1>
+  // <p>Inline template is used in this example</p>`,
   standalone: false,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'hotelInventoryApp';
+
+  role = 'Admin';
 }
